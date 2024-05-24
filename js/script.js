0632e39b58dea073f8d6bbb1009569cbb921743a
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-24 08:34:37 -0300

    arrumei algumas coisas do index e o js script

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ btnMobile.addEventListener("clique", Menu);
 /*criando o slideShow */
 
 //declarando o objeto de imagens em um array
-let imagenns = ['img/Carro1.png', 'img/Carro2.png', 'img/Carro3.png'];
+let imagens = ['img/Carro1.png', 'img/Carro2.png', 'img/Carro3.png'];
 //declarando a posição de inicio
 let Index = 0;
 //declarando o tempo que as imagens vão passar em milesegundos
@@ -36,10 +36,10 @@ function slideShow() {
     Index = 0;
   }
   //setTimeout é uma função que executa o tempo, nesse caso vai chamar função e aplicar o time
-  setTimeout('slideshow()', time);
+  setTimeout('slideShow()', time);
 }
 //executar toda a função
-slideshow();
+slideShow();
 
 
 
